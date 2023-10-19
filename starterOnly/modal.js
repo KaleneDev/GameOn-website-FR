d1acc734f666a3da0c9caae3ee3053d5e6470932
Kalène Apiou <apiou.kalene@gmail.com>
2023-10-19 08:37:38 +0200

    add check form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,8 +14,7 @@ const moddalBtnClose2 = document.querySelector(".btn-submit.closeBtn");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalBtnSumit = document.querySelector(".btn-submit");
-
-console.log(moddalBtnClose2);
+// const reserve = document.querySelector(".reserve");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -40,23 +39,16 @@ modalBtnSumit.addEventListener("click", function (e) {
     let birthdate = document.getElementById("birthdate").value;
     let quantity = document.getElementById("quantity").value;
     let location1 = document.getElementById("location1").checked;
-    // let location = document.querySelector('input[name="location"]:checked').value;
-    // let checkbox1 = document.getElementById("checkbox1").checked;
+    let location2 = document.getElementById("location2").checked;
+    let location3 = document.getElementById("location3").checked;
+    let location4 = document.getElementById("location4").checked;
+    let location5 = document.getElementById("location5").checked;
+    let location6 = document.getElementById("location6").checked;
+    let checkbox1 = document.getElementById("checkbox1").checked;
 
-    console.log(firstName, lastName);
-    // let error = document.getElementById("error");
-    // let errorFirstName = document.getElementById("errorFirstName");
-    // let errorLastName = document.getElementById("errorLastName");
-    // let errorEmail = document.getElementById("errorEmail");
-    // let errorBirthdate = document.getElementById("errorBirthdate");
-    // let errorQuantity = document.getElementById("errorQuantity");
-    // let errorLocation = document.getElementById("errorLocation");
-    // let errorCheckbox1 = document.getElementById("errorCheckbox1");
-
-    let regexName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
     let regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    if (firstName.length < 2 || !regexName.test(firstName)) {
+    if (firstName.length < 2) {
         isFormValid = false;
         errorFirstName.innerHTML =
             "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
@@ -65,7 +57,7 @@ modalBtnSumit.addEventListener("click", function (e) {
         errorFirstName.style.display = "none";
     }
 
-    if (lastName.length < 2 || !regexName.test(lastName)) {
+    if (lastName.length < 2) {
         isFormValid = false;
         errorLastName.innerHTML =
             "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
@@ -86,61 +78,47 @@ modalBtnSumit.addEventListener("click", function (e) {
         isFormValid = false;
         errorBirthdate.innerHTML = "Vous devez entrer votre date de naissance.";
         errorBirthdate.style.display = "block";
-    }
-    else {
+    } else {
         errorBirthdate.style.display = "none";
     }
 
     if (quantity == "") {
+        isFormValid = false;
         errorQuantity.innerHTML = "Vous devez entrer un nombre.";
         errorQuantity.style.display = "block";
-    }
-    else {
+    } else {
         errorQuantity.style.display = "none";
     }
 
-    if (location1 == "") {
+    if (
+        location1 === false &&
+        location2 === false &&
+        location3 === false &&
+        location4 === false &&
+        location5 === false &&
+        location6 === false
+    ) {
+        isFormValid = false;
         errorLocation1.innerHTML = "Vous devez choisir une ville.";
         errorLocation1.style.display = "block";
-   
-    }
-    else {
-        errorLocation.style.display = "none";
+    } else {
+        errorLocation1.style.display = "none";
     }
 
-    // if (!checkbox1) {
-    //     errorCheckbox1.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
-    //     errorCheckbox1.style.display = "block";
-    //     errorCheckbox1.style.color = "red";
-    //     errorCheckbox1.style.fontSize = "0.8rem";
-    //     errorCheckbox1.style.marginBottom = "0.5rem";
-    // }
-    // else {
-    //     errorCheckbox1.style.display = "none";
-    // }
-
-    // if (firstName.length >= 2 && regexName.test(firstName) && lastName.length >= 2 && regexName.test(lastName) && regexEmail.test(email) && birthdate != "" && quantity != "" && location != "" && checkbox1) {
-    //     error.style.display = "none";
-    //     document.getElementById("form").innerHTML = "<p class='success'>Merci ! Votre réservation a été reçue.</p>";
-    // }
-    // else {
-    //     error.innerHTML = "Veuillez remplir tous les champs.";
-    //     error.style.display = "block";
-    //     error.style.color = "red";
-    //     error.style.fontSize = "0.8rem";
-    //     error.style.marginBottom = "0.5rem";
-    // }
+    if (checkbox1 === false) {
+        isFormValid = false;
+        errorCheckbox1.innerHTML =
+            "Vous devez vérifier que vous acceptez les termes et conditions.";
+        errorCheckbox1.style.display = "block";
+    } else {
+        errorCheckbox1.style.display = "none";
+    }
 
     if (isFormValid) {
-        console.log("form valid");
-        const form = document.getElementById("reserve");
-        console.log(form);
         reserve.style.display = "none";
         validate.style.display = "flex";
-
         moddalBtnClose2.style.display = "flex";
     } else {
         console.log("form invalid");
     }
 });
-// if form valid
